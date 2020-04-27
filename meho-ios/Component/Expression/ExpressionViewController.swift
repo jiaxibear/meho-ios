e4d349b79feb678700af24757d762c0fa6be3fea
jiaxibear <jiaxi.xiong.us@gmail.com>
2020-04-26 18:04:08 -0700

    decorate trendingphrase

--- meho-ios/Component/Expression/ExpressionViewController.swift
+++ meho-ios/Component/Expression/ExpressionViewController.swift
@@ -8,23 +8,41 @@
 
 import UIKit
 
-class ExpressionViewController: UIViewController {
+enum ExpressionSection: Int {
+    case trendingPhrases
+}
+
+class ExpressionViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
 
     // MARK: - Constants
     private let expressionTabBarItemImageName = "tabbar_expression_25pt"
     private let foundationCoverTitle = "Expressions"
     private let titleLabelFontSize = CGFloat(34)
 
-    private let titleLabelLeadingMargin = CGFloat(15)
+    private let horizontalMargin = CGFloat(15)
     private let titleLabelTopMargin = CGFloat(30)
+    private let collectionViewTopMargin = CGFloat(24)
+
+    private let trendingCollectionViewCellSpacing = CGFloat(20)
+
+    private let sectionHeaderCellReuseIdentifier = "SectionHeaderIdentifier"
+    private let trendingPhraseCellReuseIdentifier = "TrendingPhraseCellIdentifier"
 
     // MARK: - Properties
-    private let titleView = MainTabTitleView.init(frame: .zero)
+    private lazy var titleView: MainTabTitleView = {
+        let titleView = MainTabTitleView.init(frame: .zero)
+        titleView.setTitleText(text: foundationCoverTitle)
+        titleView.translatesAutoresizingMaskIntoConstraints = false
+        return titleView
+    } ()
 
+    private var expressionCollectionViewCompositionalLayout: UICollectionViewCompositionalLayout?
+    private lazy var expressionCollectionView = UICollectionView.init(frame: .zero, collectionViewLayout:expressionCollectionViewCompositionalLayout!)
 
     // MARK: - Datamodels
     private let dataFecther = ExpressionDataFetcher.init()
-    private var trendPhrases:[Phrase] = []
+    private var trendingPhrases:[Phrase] = []
+    private var sections:[ExpressionSection] = []
 
     // MARK: - Init
     init() {
@@ -32,7 +50,16 @@ class ExpressionViewController: UIViewController {
         let expressionTabBarItemImage = UIImage.init(named: expressionTabBarItemImageName)
         let expressionTabBarItem = UITabBarItem.init(title: nil, image: expressionTabBarItemImage, tag: 0)
         tabBarItem = expressionTabBarItem
-        view.backgroundColor = .white
+
+        expressionCollectionViewCompositionalLayout = UICollectionViewCompositionalLayout { (sectionIndex: Int,
+          layoutEnvironment: NSCollectionLayoutEnvironment)
+            -> NSCollectionLayoutSection? in
+            let expressionSection = self.sections[sectionIndex]
+            switch expressionSection {
+            case .trendingPhrases:
+                return self.trendingLayoutSection()
+            }
+        }
     }
 
     @available(*, unavailable)
@@ -48,29 +75,107 @@ class ExpressionViewController: UIViewController {
     // MARK: - UIViewController
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .white
-        setupTitleView()
+        view.backgroundColor = .white
 
+        expressionCollectionView.backgroundColor = .white
+        expressionCollectionView.showsVerticalScrollIndicator = false
+        expressionCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        expressionCollectionView.register(NewsOneTitleHeaderCollectionReusableView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: sectionHeaderCellReuseIdentifier)
+        expressionCollectionView.register(TrendingPhraseCollectionViewCell.self, forCellWithReuseIdentifier: trendingPhraseCellReuseIdentifier)
+        expressionCollectionView.delegate = self
+        expressionCollectionView.dataSource = self
 
+        setupTitleViewConstraint()
+        setupExpressionCollectionView()
 
         dataFecther.fetchTrendingPhrases(completionHandler:  { (phrases, error) in
             if (error == nil && phrases != nil) {
                 DispatchQueue.main.async {
-                    self.trendPhrases = phrases!
+                    self.trendingPhrases = phrases!
+                    self.sections.insert(.trendingPhrases, at: 0) // Change to 1 once survival section added
+                    self.expressionCollectionView.reloadData()
                 }
             }
         })
+
     }
 
-    func setupTitleView() {
+    private func setupTitleViewConstraint() {
         let margins = self.view.layoutMarginsGuide
-        titleView.setTitleText(text: foundationCoverTitle)
-        titleView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(titleView)
-
-        titleView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: titleLabelLeadingMargin).isActive = true
-        titleView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -titleLabelLeadingMargin).isActive = true
+        titleView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: horizontalMargin).isActive = true
+        titleView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -horizontalMargin).isActive = true
         titleView.topAnchor.constraint(equalTo: margins.topAnchor, constant: titleLabelTopMargin).isActive = true
-//        titleView.heightAnchor.constraint(equalToConstant: titleView.getViewHeight()).isActive = true
+    }
+
+    private func setupExpressionCollectionView() {
+        view.addSubview(expressionCollectionView)
+
+        expressionCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: horizontalMargin).isActive = true
+        expressionCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -horizontalMargin).isActive = true
+        expressionCollectionView.topAnchor.constraint(equalTo: titleView.bottomAnchor, constant: collectionViewTopMargin).isActive = true
+        expressionCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
+
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        let expressionSections = sections[section]
+        switch expressionSections {
+        case .trendingPhrases:
+            return trendingPhrases.count
+        }
+    }
+
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return sections.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let expressionSection = sections[indexPath.section]
+        switch expressionSection {
+        case .trendingPhrases:
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: trendingPhraseCellReuseIdentifier, for: indexPath) as! TrendingPhraseCollectionViewCell
+            let phrase = trendingPhrases[indexPath.item]
+            cell.setPhrase(phrase)
+            return cell
+        }
+    }
+
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        if kind == UICollectionView.elementKindSectionHeader {
+            let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: sectionHeaderCellReuseIdentifier, for: indexPath) as! NewsOneTitleHeaderCollectionReusableView
+            headerView.setTitle(titleEn: "Trending Phrase")
+            return headerView
+        }
+        return UICollectionReusableView.init(frame: .zero)
+    }
+
+    // NOT CALLED!!!!
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let expressionSection = sections[indexPath.section]
+        switch expressionSection {
+        case .trendingPhrases:
+            let width = collectionView.bounds.width
+            let phrase = trendingPhrases[indexPath.item]
+            let height = TrendingPhraseCollectionViewCell.cellHeight(with: width, phrase: phrase)
+            return CGSize.init(width: width, height: height)
+        }
+    }
+
+
+    // MARK: - Section Layouts
+    private func trendingLayoutSection() -> NSCollectionLayoutSection {
+        let cellWidth = self.view.bounds.width - 2 * horizontalMargin
+        let itemSize = NSCollectionLayoutSize.init(widthDimension: .absolute(cellWidth), heightDimension: .estimated(CGFloat(200)))
+        let item = NSCollectionLayoutItem.init(layoutSize: itemSize)
+        let groupSize = NSCollectionLayoutSize.init(widthDimension: .absolute(cellWidth), heightDimension: .estimated(CGFloat(1000)))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 1)
+        let section = NSCollectionLayoutSection.init(group: group)
+
+        section.interGroupSpacing = trendingCollectionViewCellSpacing
+        let headerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(CGFloat(30)))
+        let headerElement = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: headerSize, elementKind: UICollectionView.elementKindSectionHeader, alignment: .top)
+        section.boundarySupplementaryItems = [headerElement]
+        return section
     }
 }
---
+++ meho-ios/Component/Expression/TrendingPhraseCollectionViewCell.swift
@@ -0,0 +1,170 @@
+//
+//  TrendingPhraseCollectionViewCell.swift
+//  meho-ios
+//
+//  Created by Jiaxi Xiong on 4/26/20.
+//  Copyright © 2020 Meho. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+
+class TrendingPhraseCollectionViewCell: UICollectionViewCell {
+    // MARK: - Constants
+    private static let elementHorizontalMargin = CGFloat(18)
+    private static let phraseLabelFontSize = CGFloat(20)
+    private static let pinyinLabelFontSize = CGFloat(18)
+    private static let explanationLabelFontSize = CGFloat(16)
+    private static let labelToExplainMargin = CGFloat(10)
+    private static let contentViewCornerRadius = CGFloat(8)
+    private static let contentViewShadowRadius = CGFloat(2)
+    private static let contentViewShadowSpread = CGFloat(2)
+
+    private static let pronounceSpeakerSize = CGFloat(25)
+    private static let cellVerticalMargin = CGFloat(7)
+    private static let pronounceButtonImageName = "stories_speaker"
+
+    // MARK: - Properties
+    private lazy var phraseLabel:UILabel = {
+        let label = UILabel.init(frame: .zero)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .wisteriaPurple
+        label.font = UIFont.init(name: "PingFangSC-Semibold", size: TrendingPhraseCollectionViewCell.phraseLabelFontSize)
+        label.numberOfLines = 1
+        label.backgroundColor = .paleLilac
+        return label
+    } ()
+
+    private lazy var pinyinLabel:UILabel = {
+        let label = UILabel.init(frame: .zero)
+        label.translatesAutoresizingMaskIntoConstraints = false
+
+        label.textColor = .darkGray
+        let pinyinfontDescriptor = UIFont.systemFont(ofSize: TrendingPhraseCollectionViewCell.pinyinLabelFontSize, weight: .regular).fontDescriptor.withDesign(.rounded)
+        label.font = UIFont.init(descriptor: pinyinfontDescriptor!, size: 0)
+        label.numberOfLines = 1
+        return label
+    } ()
+
+    private lazy var prounceButton: UIButton = {
+        let button = UIButton.init(frame: .zero)
+        let pronounceSpeakerImage = UIImage.init(named: TrendingPhraseCollectionViewCell.pronounceButtonImageName)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(pronounceSpeakerImage, for: UIControl.State.normal)
+        button.setImage(pronounceSpeakerImage, for: UIControl.State.selected)
+        button.addTarget(self, action: #selector(didTapPronounceButton), for: .touchUpInside)
+        return button
+    } ()
+
+    private lazy var explanationLabel:UILabel = {
+        let label = UILabel.init(frame: .zero)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .darkGray
+        let fontDescriptor = UIFont.systemFont(ofSize: TrendingPhraseCollectionViewCell.explanationLabelFontSize, weight: .regular).fontDescriptor.withDesign(.rounded)
+        label.font = UIFont.init(descriptor: fontDescriptor!, size: 0)
+        label.numberOfLines = 0
+        return label
+    } ()
+
+    private static var sizingCell = TrendingPhraseCollectionViewCell.init(frame: .zero);
+
+    // MARK: - Data
+    private var player: AVPlayer?
+    private var maybePronounceAudioUrl: URL?
+
+
+    // MARK: - Init
+    @available(*, unavailable)
+    init() {
+        fatalError("Use init(frame: CGRect)")
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("Use init(frame: CGRect)")
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        layer.shadowColor = UIColor.paleLilac.cgColor
+        layer.shadowOpacity = 0.5
+        layer.shadowOffset = .zero
+        layer.masksToBounds = false
+        layer.shadowRadius = TrendingPhraseCollectionViewCell.contentViewShadowRadius
+        let shadowBounds = bounds.insetBy(dx: TrendingPhraseCollectionViewCell.contentViewShadowSpread, dy: -TrendingPhraseCollectionViewCell.contentViewShadowSpread)
+        layer.shadowPath = UIBezierPath(roundedRect: shadowBounds, cornerRadius: TrendingPhraseCollectionViewCell.contentViewCornerRadius).cgPath
+        layer.backgroundColor = UIColor.clear.cgColor
+
+        contentView.layer.borderWidth = 0.5
+        contentView.layer.borderColor = UIColor.paleLilac.cgColor
+        contentView.layer.masksToBounds = true
+        contentView.layer.cornerRadius = TrendingPhraseCollectionViewCell.contentViewCornerRadius
+        contentView.backgroundColor = .white
+
+        setupPhraseLabel()
+        setupPinyinLabel()
+        setupPronounceButton()
+        setupExplanationLabel()
+
+    }
+
+    private func setupPhraseLabel() {
+        contentView.addSubview(phraseLabel)
+
+        phraseLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: TrendingPhraseCollectionViewCell.elementHorizontalMargin).isActive = true
+        phraseLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: TrendingPhraseCollectionViewCell.cellVerticalMargin).isActive = true
+    }
+
+    private func setupPinyinLabel() {
+        contentView.addSubview(pinyinLabel)
+
+        pinyinLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: TrendingPhraseCollectionViewCell.elementHorizontalMargin).isActive = true
+        pinyinLabel.topAnchor.constraint(equalTo: phraseLabel.bottomAnchor, constant: TrendingPhraseCollectionViewCell.cellVerticalMargin).isActive = true
+    }
+
+    private func setupPronounceButton() {
+        contentView.addSubview(prounceButton)
+
+        prounceButton.leadingAnchor.constraint(equalTo: pinyinLabel.trailingAnchor).isActive = true
+        prounceButton.centerYAnchor.constraint(equalTo: pinyinLabel.centerYAnchor).isActive = true
+        prounceButton.widthAnchor.constraint(equalToConstant: TrendingPhraseCollectionViewCell.pronounceSpeakerSize).isActive = true
+        prounceButton.heightAnchor.constraint(equalToConstant: TrendingPhraseCollectionViewCell.pronounceSpeakerSize).isActive = true
+    }
+
+    private func setupExplanationLabel() {
+        contentView.addSubview(explanationLabel)
+
+        explanationLabel.topAnchor.constraint(equalTo: pinyinLabel.bottomAnchor, constant: TrendingPhraseCollectionViewCell.cellVerticalMargin).isActive = true
+        explanationLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -TrendingPhraseCollectionViewCell.cellVerticalMargin).isActive = true
+        explanationLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: TrendingPhraseCollectionViewCell.elementHorizontalMargin).isActive = true
+        explanationLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -TrendingPhraseCollectionViewCell.elementHorizontalMargin).isActive = true
+    }
+
+    // MARK: - Private
+    @objc func didTapPronounceButton() {
+        if let pronounceURL = maybePronounceAudioUrl {
+            let playerItem = AVPlayerItem.init(url: pronounceURL)
+            player = AVPlayer.init(playerItem: playerItem)
+            player?.rate = AudioPlaySpeed.normal.rawValue
+            player?.play()
+        }
+    }
+
+    // MARK: - Public
+    public func setPhrase(_ phrase: Phrase) {
+        phraseLabel.text = " # " + phrase.content_zh + " "
+        pinyinLabel.text = "/" + phrase.content_pinyin + "/  "
+        explanationLabel.text = phrase.content_explanation
+        maybePronounceAudioUrl = phrase.audioURL
+    }
+
+    public class func cellHeight(with width: CGFloat, phrase: Phrase) -> CGFloat {
+        sizingCell.setPhrase(phrase)
+        let height = TrendingPhraseCollectionViewCell.cellVerticalMargin * 4
+            + sizingCell.phraseLabel.sizeThatFits(CGSize.init(width: width, height: .greatestFiniteMagnitude)).height
+            + sizingCell.pinyinLabel.sizeThatFits(CGSize.init(width: width, height: .greatestFiniteMagnitude)).height
+            + sizingCell.explanationLabel.sizeThatFits(CGSize.init(width: width, height: .greatestFiniteMagnitude)).height
+        return height
+    }
+}
